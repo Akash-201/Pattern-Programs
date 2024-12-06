9a3ef6fc1e967cc1ac542879ced2219891ba31fb
AKASH KUMAR <141420638+Akash-201@users.noreply.github.com>
2024-12-06 23:32:40 +0530

    Add files via upload

---
+++ Voting/src/Voting.java
@@ -0,0 +1,14 @@
+
+public class Voting
+{
+	public static void main(String[] args)
+	{
+		int age = 19;
+		System.out.println("start");
+		if(age>=18);
+		{
+			System.out.println("voting");
+		}
+		System.out.println("end");
+	}
+}
